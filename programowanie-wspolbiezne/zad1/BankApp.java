b168ff4bd424ff6a775305dddd712998278428ac
Haswell33 <31355807+Haswell33@users.noreply.github.com>
2021-11-11 14:34:54 +0100

    Delete BankApp.java

--- programowanie-wspolbiezne/zad1/BankApp.java
+++
@@ -1,8 +0,0 @@
-public class BankApp {
-
-/*for(int i = 0; i < Bank.ilosc_kont; i++){
-        Transfer transfer_1 = new Transfer(konto_1,);
-        Thread thread_1 = new Thread(transfer_1);
-        thread_1.start();
-    }*/
-}
