cb02003bc8e6c312c1223c38f6d91b4807c44bcf
Haswell33 <31355807+Haswell33@users.noreply.github.com>
2021-11-12 20:57:29 +0100

    Add files via upload

---
+++ programowanie-wspolbiezne/lab4/src/Filozof.java
@@ -0,0 +1,37 @@
+public class Filozof implements Runnable{
+    String nr;
+    Paleczka lewa;
+    Paleczka prawa;
+
+    Object lewa_paleczka;
+    Object prawa_paleczka;
+
+    public Filozof(Paleczka lewa, Paleczka prawa){
+        this.lewa_paleczka = lewa_paleczka;
+        this.prawa_paleczka = prawa_paleczka;
+    }
+
+    void czekaj(){
+
+    }
+
+    void podnies_lewa_paleczke(){
+
+    }
+
+    void podnies_prawa_paleczke(){
+
+    }
+
+    void jedz(){
+
+    }
+
+    void odloz_lewa_paleczke(){
+
+    }
+
+    void odloz_prawa_paleczke(){
+
+    }
+}
---
+++ programowanie-wspolbiezne/lab4/src/Main.java
@@ -0,0 +1,7 @@
+// Karol Siedlaczek I8X1N1 2019
+// Zaimplementuj problem ucztujących filozofów
+public class Main {
+    static public void main(String args[]) throws InterruptedException {
+
+    }
+}
